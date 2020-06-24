787d0e4e3f0e59acc7abd2d6f4962de2269d41f9
York <anyone.1984@gmail.com>
2020-06-25 00:05:37 +0800

    清理冗余代码

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # FastScroll
-A fast-scroll component for wechat miniprogram.
+A fast-scroll component for wechat miniprogram.  
+一个仿微信好友列表的小程序快捷滚动条组件。
+
+## Preview
+![Preview 1](images/1.gif "点击和划动")
+![Preview 2](images/2.gif "监听列表滚动")
+
+## Reference
+- [wxaSortPicker](https://github.com/nickrogit/wxaSortPicker)
+- [Javascript 汉字转首字母的拼音](https://www.iteye.com/blog/wuxiangqian-1426339)
--- mock-data.js
+++ mock-data.js
@@ -1,403 +1,503 @@
 const data = [
   {
-    "name": "Mojito",
-    "album": "Mojito"
-  },
-  {
-    "name": "微微",
-    "album": "微微"
-  },
-  {
-    "name": "无价之姐",
-    "album": "无价之姐"
-  },
-  {
-    "name": "想见你想见你想见你",
-    "album": "乐队无限公司 第13期"
-  },
-  {
-    "name": "Sour Candy",
-    "album": "Chromatica"
-  },
-  {
-    "name": "Left & Right",
-    "album": "SEVENTEEN 7th Mini Album '헹가래'"
-  },
-  {
-    "name": "怪女孩 Miss Freak (纯享版)",
-    "album": "创造营2020 第8期 (纯享版)"
-  },
-  {
-    "name": "借画",
-    "album": "借画"
-  },
-  {
-    "name": "离群 (纯享版)",
-    "album": "创造营2020 第8期 (纯享版)"
-  },
-  {
-    "name": "探清水河",
-    "album": "探清水河"
-  },
-  {
-    "name": "冰激凌",
-    "album": "冰激凌"
-  },
-  {
-    "name": "5452830",
-    "album": "遇见•再见"
-  },
-  {
-    "name": "Far Away 遥远",
-    "album": "乐队无限公司 第13期"
-  },
-  {
-    "name": "在一起嘛好不好",
-    "album": "在一起嘛好不好"
-  },
-  {
-    "name": "去爱你",
-    "album": "去爱你"
-  },
-  {
-    "name": "Ice Queen (纯享版)",
-    "album": "创造营2020 第8期 (纯享版)"
-  },
-  {
-    "name": "着迷 Lost in ME (纯享版)",
-    "album": "创造营2020 第8期 (纯享版)"
-  },
-  {
-    "name": "Ready Now",
-    "album": "Ready Now"
-  },
-  {
-    "name": "皆可",
-    "album": "皆可"
-  },
-  {
-    "name": "Fearless",
-    "album": "SEVENTEEN 7th Mini Album '헹가래'"
+    "name": "회전목마 (Merry-Go-Round)",
+    "album": "Oneiric Diary (幻想日记)",
+    "pic": "../../images/260x260.png"
   },
   {
     "name": "환상동화 (Secret Story of the Swan)",
-    "album": "Oneiric Diary (幻想日记)"
-  },
-  {
-    "name": "世界不会轻易崩塌 (纯享版)",
-    "album": "创造营2020 第5期 (纯享版)"
-  },
-  {
-    "name": "时光话",
-    "album": "传闻中的陈芊芊 网剧原声大碟"
-  },
-  {
-    "name": "圆舞曲",
-    "album": "圆舞曲"
-  },
-  {
-    "name": "My My",
-    "album": "SEVENTEEN 7th Mini Album '헹가래'"
-  },
-  {
-    "name": "未来日记",
-    "album": "未来日记"
-  },
-  {
-    "name": "Cure The World",
-    "album": "Risk It All 声来无畏"
-  },
-  {
-    "name": "MORE & MORE",
-    "album": "MORE & MORE"
+    "album": "Oneiric Diary (幻想日记)",
+    "pic": "../../images/260x260.png"
   },
   {
     "name": "좋겠다 (I Wish)",
-    "album": "SEVENTEEN 7th Mini Album '헹가래'"
+    "album": "SEVENTEEN 7th Mini Album '헹가래'",
+    "pic": "../../images/260x260.png"
   },
   {
     "name": "어른 아이 (Kidult)",
-    "album": "SEVENTEEN 7th Mini Album '헹가래'"
-  },
-  {
-    "name": "每天起来唱一遍 (纯享版)",
-    "album": "创造营2020 第8期 (纯享版)"
-  },
-  {
-    "name": "缘落",
-    "album": "月上重火 电视剧原声大碟"
-  },
-  {
-    "name": "孤岛",
-    "album": "孤岛"
-  },
-  {
-    "name": "都怪我",
-    "album": "都怪我"
-  },
-  {
-    "name": "那女孩对我说 (纯享版)",
-    "album": "创造营2020 第5期 (纯享版)"
-  },
-  {
-    "name": "这是我一直想对你说的话 (纯享版)",
-    "album": "创造营2020 第8期 (纯享版)"
-  },
-  {
-    "name": "硬糖",
-    "album": "遇见•再见"
-  },
-  {
-    "name": "缘起",
-    "album": "月上重火 电视剧原声大碟"
+    "album": "SEVENTEEN 7th Mini Album '헹가래'",
+    "pic": "../../images/260x260.png"
   },
   {
     "name": "같이 가요 (Together)",
-    "album": "SEVENTEEN 7th Mini Album '헹가래'"
-  },
-  {
-    "name": "呼吸",
-    "album": "呼吸"
-  },
-  {
-    "name": "嘘！我跟你讲 ",
-    "album": "遇见•再见"
-  },
-  {
-    "name": "淘汰",
-    "album": "淘汰"
-  },
-  {
-    "name": "忽如一面",
-    "album": "忽如一面"
-  },
-  {
-    "name": "我陪你熬过了苦",
-    "album": "我陪你熬过了苦"
-  },
-  {
-    "name": "You're Cold (더 많이 사랑한 쪽이 아프대)",
-    "album": "사이코지만 괜찮아 OST Part.1 (虽然是精神病但没关系 OST Part.1)"
-  },
-  {
-    "name": "不易",
-    "album": "不易"
-  },
-  {
-    "name": "字里行间",
-    "album": "民初奇人传 网络剧原声带"
-  },
-  {
-    "name": "一梦情深",
-    "album": "一梦情深"
-  },
-  {
-    "name": "分分钟需要你",
-    "album": "分分钟需要你"
-  },
-  {
-    "name": "情人咒",
-    "album": "情人咒"
-  },
-  {
-    "name": "俏才郎",
-    "album": "俏才郎"
-  },
-  {
-    "name": "一步成诗 (纯享版)",
-    "album": "创造营2020 第5期 (纯享版)"
-  },
-  {
-    "name": "夏天",
-    "album": "某年夏天"
-  },
-  {
-    "name": "晚睡患者",
-    "album": "晚睡患者"
-  },
-  {
-    "name": "曜",
-    "album": "曜为名"
-  },
-  {
-    "name": "11次心跳",
-    "album": "遇见•再见"
-  },
-  {
-    "name": " 超时空 回 (Turn Back Time)",
-    "album": "Awaken The World - The 1st Album"
-  },
-  {
-    "name": "夏天的风 (纯享版)",
-    "album": "创造营2020 第5期 (纯享版)"
-  },
-  {
-    "name": "不必在乎我是谁",
-    "album": "不必在乎我是谁"
-  },
-  {
-    "name": "蝴蝶飓风",
-    "album": "蝴蝶飓风（《怪你过分美丽》电视剧主题曲）"
-  },
-  {
-    "name": "莲 (Lit)",
-    "album": "莲"
-  },
-  {
-    "name": "意中人",
-    "album": "意中人"
-  },
-  {
-    "name": "慢慢 (Slow Motion)",
-    "album": "π-volume.3"
-  },
-  {
-    "name": "River (纯享版)",
-    "album": "创造营2020 第5期 (纯享版)"
-  },
-  {
-    "name": "白首",
-    "album": "暮白首 电视剧原声带"
-  },
-  {
-    "name": "不红",
-    "album": "不红"
-  },
-  {
-    "name": "对心 (纯享版)",
-    "album": "创造营2020 第8期 (纯享版)"
-  },
-  {
-    "name": "她们",
-    "album": "她们"
-  },
-  {
-    "name": "Candy",
-    "album": "Delight - The 2nd Mini Album"
-  },
-  {
-    "name": "若岸",
-    "album": "若岸"
-  },
-  {
-    "name": "先生呀",
-    "album": "先生呀"
-  },
-  {
-    "name": "逝言",
-    "album": "暮白首 电视剧原声带"
-  },
-  {
-    "name": "每天起来唱一遍",
-    "album": "每天起来唱一遍"
-  },
-  {
-    "name": "天如水",
-    "album": "天如水"
-  },
-  {
-    "name": "莫测",
-    "album": "月上重火 电视剧原声大碟"
-  },
-  {
-    "name": "往前",
-    "album": "往前"
-  },
-  {
-    "name": "耿耿于怀",
-    "album": "耿耿于怀"
-  },
-  {
-    "name": "Welcome",
-    "album": "Oneiric Diary (幻想日记)"
-  },
-  {
-    "name": "六七月",
-    "album": "我才不要和你做朋友呢 影视剧原声带"
-  },
-  {
-    "name": "Pretty",
-    "album": "Oneiric Diary (幻想日记)"
-  },
-  {
-    "name": "少年之名",
-    "album": "少年之名"
-  },
-  {
-    "name": "落空",
-    "album": "落空"
-  },
-  {
-    "name": "회전목마 (Merry-Go-Round)",
-    "album": "Oneiric Diary (幻想日记)"
-  },
-  {
-    "name": "Selfish (Alan Walker Remix)",
-    "album": "Selfish (Alan Walker Remix)"
-  },
-  {
-    "name": "还是一个人",
-    "album": "还是一个人"
-  },
-  {
-    "name": "灵 (Soul)",
-    "album": "莲"
-  },
-  {
-    "name": "911",
-    "album": "Chromatica"
-  },
-  {
-    "name": "水 (H2O)",
-    "album": "莲"
-  },
-  {
-    "name": "青春就这Young",
-    "album": "青春环游记第二季 原声碟"
-  },
-  {
-    "name": "相遇花开",
-    "album": "相遇花开"
-  },
-  {
-    "name": "Hello (录音室版)",
-    "album": "Hello (录音室版)"
-  },
-  {
-    "name": "太空迪斯科",
-    "album": "太空迪斯科"
-  },
-  {
-    "name": "Marry Me",
-    "album": "C-POP"
-  },
-  {
-    "name": "闲庭絮",
-    "album": "闲庭絮"
-  },
-  {
-    "name": "亲爱的，你好",
-    "album": "亲爱的，你好"
-  },
-  {
-    "name": "空白以后",
-    "album": "我才不要和你做朋友呢 影视剧原声带"
+    "album": "SEVENTEEN 7th Mini Album '헹가래'",
+    "pic": "../../images/260x260.png"
   },
   {
     "name": "Youngblood (드라이브 인 버스킹 Ver.)",
-    "album": "JTBC 비긴어게인 코리아 Episode.1 (JTBC Begin Again Korea Episode.1)"
+    "album": "JTBC 비긴어게인 코리아 Episode.1 (JTBC Begin Again Korea Episode.1)",
+    "pic": "../../images/260x260.png"
   },
   {
-    "name": "就让我忘了曾经的那个人",
-    "album": "就让我忘了曾经的那个人"
+    "name": "You're Cold (더 많이 사랑한 쪽이 아프대)",
+    "album": "사이코지만 괜찮아 OST Part.1 (虽然是精神病但没关系 OST Part.1)",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "Welcome",
+    "album": "Oneiric Diary (幻想日记)",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "Sour Candy",
+    "album": "Chromatica",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "Selfish (Alan Walker Remix)",
+    "album": "Selfish (Alan Walker Remix)",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "River (纯享版)",
+    "album": "创造营2020 第5期 (纯享版)",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "Ready Now",
+    "album": "Ready Now",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "Pretty",
+    "album": "Oneiric Diary (幻想日记)",
+    "pic": "../../images/260x260.png"
   },
   {
     "name": "Only Human",
-    "album": "Awaken The World - The 1st Album"
+    "album": "Awaken The World - The 1st Album",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "My My",
+    "album": "SEVENTEEN 7th Mini Album '헹가래'",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "MORE & MORE",
+    "album": "MORE & MORE",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "Mojito",
+    "album": "Mojito",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "Marry Me",
+    "album": "C-POP",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "Left & Right",
+    "album": "SEVENTEEN 7th Mini Album '헹가래'",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "Ice Queen (纯享版)",
+    "album": "创造营2020 第8期 (纯享版)",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "Hello (录音室版)",
+    "album": "Hello (录音室版)",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "Fearless",
+    "album": "SEVENTEEN 7th Mini Album '헹가래'",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "Far Away 遥远",
+    "album": "乐队无限公司 第13期",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "Cure The World",
+    "album": "Risk It All 声来无畏",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "Candy",
+    "album": "Delight - The 2nd Mini Album",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "字里行间",
+    "album": "民初奇人传 网络剧原声带",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "着迷 Lost in ME (纯享版)",
+    "album": "创造营2020 第8期 (纯享版)",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "这是我一直想对你说的话 (纯享版)",
+    "album": "创造营2020 第8期 (纯享版)",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "在一起嘛好不好",
+    "album": "在一起嘛好不好",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "缘起",
+    "album": "月上重火 电视剧原声大碟",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "缘落",
+    "album": "月上重火 电视剧原声大碟",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "圆舞曲",
+    "album": "圆舞曲",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "硬糖",
+    "album": "遇见•再见",
+    "pic": "../../images/260x260.png"
   },
   {
     "name": "鹰 (Eagle)",
-    "album": "莲"
+    "album": "莲",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "意中人",
+    "album": "意中人",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "一梦情深",
+    "album": "一梦情深",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "一步成诗 (纯享版)",
+    "album": "创造营2020 第5期 (纯享版)",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "曜",
+    "album": "曜为名",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "嘘！我跟你讲",
+    "album": "遇见•再见",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "想见你想见你想见你",
+    "album": "乐队无限公司 第13期",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "相遇花开",
+    "album": "相遇花开",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "闲庭絮",
+    "album": "闲庭絮",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "先生呀",
+    "album": "先生呀",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "夏天的风 (纯享版)",
+    "album": "创造营2020 第5期 (纯享版)",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "夏天",
+    "album": "某年夏天",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "无价之姐",
+    "album": "无价之姐",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "我陪你熬过了苦",
+    "album": "我陪你熬过了苦",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "未来日记",
+    "album": "未来日记",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "微微",
+    "album": "微微",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "往前",
+    "album": "往前",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "晚睡患者",
+    "album": "晚睡患者",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "天如水",
+    "album": "天如水",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "淘汰",
+    "album": "淘汰",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "探清水河",
+    "album": "探清水河",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "太空迪斯科",
+    "album": "太空迪斯科",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "她们",
+    "album": "她们",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "水 (H2O)",
+    "album": "莲",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "逝言",
+    "album": "暮白首 电视剧原声带",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "世界不会轻易崩塌 (纯享版)",
+    "album": "创造营2020 第5期 (纯享版)",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "时光话",
+    "album": "传闻中的陈芊芊 网剧原声大碟",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "少年之名",
+    "album": "少年之名",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "若岸",
+    "album": "若岸",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "去爱你",
+    "album": "去爱你",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "情人咒",
+    "album": "情人咒",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "青春就这Young",
+    "album": "青春环游记第二季 原声碟",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "亲爱的，你好",
+    "album": "亲爱的，你好",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "俏才郎",
+    "album": "俏才郎",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "那女孩对我说 (纯享版)",
+    "album": "创造营2020 第5期 (纯享版)",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "莫测",
+    "album": "月上重火 电视剧原声大碟",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "每天起来唱一遍 (纯享版)",
+    "album": "创造营2020 第8期 (纯享版)",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "每天起来唱一遍",
+    "album": "每天起来唱一遍",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "慢慢 (Slow Motion)",
+    "album": "π-volume.3",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "落空",
+    "album": "落空",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "六七月",
+    "album": "我才不要和你做朋友呢 影视剧原声带",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "灵 (Soul)",
+    "album": "莲",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "莲 (Lit)",
+    "album": "莲",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "离群 (纯享版)",
+    "album": "创造营2020 第8期 (纯享版)",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "空白以后",
+    "album": "我才不要和你做朋友呢 影视剧原声带",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "就让我忘了曾经的那个人",
+    "album": "就让我忘了曾经的那个人",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "借画",
+    "album": "借画",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "皆可",
+    "album": "皆可",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "蝴蝶飓风",
+    "album": "蝴蝶飓风（《怪你过分美丽》电视剧主题曲）",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "忽如一面",
+    "album": "忽如一面",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "呼吸",
+    "album": "呼吸",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "还是一个人",
+    "album": "还是一个人",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "怪女孩 Miss Freak (纯享版)",
+    "album": "创造营2020 第8期 (纯享版)",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "孤岛",
+    "album": "孤岛",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "耿耿于怀",
+    "album": "耿耿于怀",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "分分钟需要你",
+    "album": "分分钟需要你",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "对心 (纯享版)",
+    "album": "创造营2020 第8期 (纯享版)",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "都怪我",
+    "album": "都怪我",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "不易",
+    "album": "不易",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "不红",
+    "album": "不红",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "不必在乎我是谁",
+    "album": "不必在乎我是谁",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "冰激凌",
+    "album": "冰激凌",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "白首",
+    "album": "暮白首 电视剧原声带",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "911",
+    "album": "Chromatica",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "5452830",
+    "album": "遇见•再见",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "11次心跳",
+    "album": "遇见•再见",
+    "pic": "../../images/260x260.png"
+  },
+  {
+    "name": "超时空 回 (Turn Back Time)",
+    "album": "Awaken The World - The 1st Album",
+    "pic": "../../images/260x260.png"
   }
 ]
 
